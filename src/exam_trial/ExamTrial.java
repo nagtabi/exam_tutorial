0d3dff158e9c12cb76e8405569b7fe2f4fd3cff1
Gaborov <nagy.gabor70@gmail.com>
2023-02-14 13:05:54 +0100

    first commit

---
+++ src/exam_trial/ExamTrial.java
@@ -0,0 +1,19 @@
+package exam_trial;
+
+/**
+ * @author --G--
+ */
+public class ExamTrial {
+
+    public static void main(String[] args) {
+        int bitCount = bitCount(31);
+
+    }
+
+    private static int bitCount(int i) {
+        //your solution here
+        //how many 1 bit is in the attributte?
+        throw new UnsupportedOperationException("exam failed.");
+    }
+
+}

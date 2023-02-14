14f809a1934e97e800bdab2056b07da078b351fd
Karesz93 <skymed012@gmail.com>
2023-02-14 13:40:48 +0100

    ready

--- src/exam_trial/ExamTrial.java
+++ src/exam_trial/ExamTrial.java
@@ -7,13 +7,18 @@ public class ExamTrial {
 
     public static void main(String[] args) {
         int bitCount = bitCount(31);
+        System.out.println(bitCount);
 
     }
 
     private static int bitCount(int i) {
-        //your solution here
-        //how many 1 bit is in the attributte?
-        throw new UnsupportedOperationException("exam failed.");
+        int count = 0;
+        while (i != 0) {
+            if ((i & 1) == 1) {
+                count++;
+            }
+            i >>>= 1;
+        }
+        return count;
     }
-
 }
